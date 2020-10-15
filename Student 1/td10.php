f680012ebd48217de53d7598d23a43f29fd166d2
Bastien Cordier <72811369+bastien-cordier@users.noreply.github.com>
2020-10-15 17:56:22 +0200

    Add files via upload

---
+++ Student 1/td10.php
@@ -0,0 +1,13 @@
+<?php
+$user = [
+    "firstname" => "Bastien",
+    "lastname" => "Cordier",
+    "age" => "21",
+    "job" => "Dev",
+];
+echo $user['firstname']."<br>"; 
+echo $user['lastname']."<br>";
+echo $user['age']."<br>"; 
+echo $user['job']."<br>";
+
+?>
---
+++ Student 1/td5.php
@@ -0,0 +1,6 @@
+<?php
+
+$age = 21;
+
+echo ($age > 21) ? "oui" : "non";
+?>
